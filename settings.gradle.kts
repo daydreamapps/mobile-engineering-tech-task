a6065895c2458607db63aca0505352eb428178e7
John Doe <john.doe@example.com>
2026-07-28 08:41:12 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+rootProject.name = "UserHub"
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+include(":domain")
+include(":data")
+include(":composeApp")
