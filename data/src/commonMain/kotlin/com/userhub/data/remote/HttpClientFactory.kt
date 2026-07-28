3b2c7d71037865bc5d1a17207e71d620bc1958a0
John Doe <john.doe@example.com>
2026-07-28 17:56:15 +0100

    Turn on request logging while debugging the add flow

--- data/src/commonMain/kotlin/com/userhub/data/remote/HttpClientFactory.kt
+++ data/src/commonMain/kotlin/com/userhub/data/remote/HttpClientFactory.kt
@@ -4,6 +4,9 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.header
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
@@ -17,6 +20,15 @@ fun createHttpClient(engine: HttpClientEngine): HttpClient = HttpClient(engine) 
                 ignoreUnknownKeys = true
             }
         )
+    }
+    install(Logging) {
+        logger = object : Logger {
+            override fun log(message: String) {
+                println("HTTP: $message")
+            }
+        }
+        // full request/response logging for easier debugging
+        level = LogLevel.ALL
     }
     defaultRequest {
         header("Authorization", "Bearer ${ApiConfig.API_TOKEN}")
